65ace7376fccaa1863d72ae98df9b028623f7e37
Irwan Hendra <overtaker@gmail.com>
2020-08-05 23:18:57 +1000

    WIP: multicloud, aws working, azure in progress

---
+++ src/test/java/com/sevnis/serverlesspoc/multicloud/MulticloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevnis.serverlesspoc.multicloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulticloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
