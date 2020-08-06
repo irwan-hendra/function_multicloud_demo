8ce12d2d34ef40ec819f6c15e443073745b12367
Irwan Hendra <overtaker@gmail.com>
2020-08-07 00:44:36 +1000

    WIP: azure working, fix shadowjar naming

--- src/main/java/com/sevnis/serverlesspoc/multicloud/MulticloudApplication.java
+++ src/main/java/com/sevnis/serverlesspoc/multicloud/MulticloudApplication.java
@@ -1,34 +1,19 @@
 package com.sevnis.serverlesspoc.multicloud;
 
 import java.util.function.Function;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.cloud.function.context.FunctionalSpringApplication;
-import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-public class MulticloudApplication {
+@SpringBootConfiguration
+public class MulticloudApplication implements Function<String, String> {
 
   public static void main(String[] args) {
     FunctionalSpringApplication.run(MulticloudApplication.class, args);
   }
 
-  @Bean
-  public Function<String, String> uppercase() {
-    return value -> value.toUpperCase();
+  @Override
+  public String apply(String value) {
+    return value.toUpperCase();
   }
 
 }
-
-//@SpringBootConfiguration
-//public class MulticloudApplication implements Function<String, String> {
-//
-//	public static void main(String[] args) {
-//		FunctionalSpringApplication.run(MulticloudApplication.class, args);
-//	}
-//
-//	@Override
-//	public String apply(String value) {
-//		return value.toUpperCase();
-//	}
-//
-//}
--- src/main/java/com/sevnis/serverlesspoc/multicloud/UppercaseHandler.java
+++ src/main/java/com/sevnis/serverlesspoc/multicloud/UppercaseHandler.java
@@ -6,21 +6,18 @@ import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
-
 import java.util.Optional;
-
 import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
 
 
 public class UppercaseHandler extends AzureSpringBootRequestHandler<String, String> {
 
-	@FunctionName("uppercase")
-	public String execute(@HttpTrigger(name = "req",
-			methods = {HttpMethod.GET, HttpMethod.POST},
-			authLevel = AuthorizationLevel.ANONYMOUS)
-			HttpRequestMessage<Optional<String>> request,
-			ExecutionContext context) {
-		return handleRequest(request.getBody().get(), context);
-	}
-
+  @FunctionName("uppercase")
+  public String execute(@HttpTrigger(name = "req",
+      methods = {HttpMethod.GET, HttpMethod.POST},
+      authLevel = AuthorizationLevel.ANONYMOUS)
+      HttpRequestMessage<Optional<String>> request,
+      ExecutionContext context) {
+    return handleRequest(request.getBody().get(), context);
+  }
 }
